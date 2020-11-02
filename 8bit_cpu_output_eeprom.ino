40085a05a787fcb0f1b1312229eda19eec15f6fd
Marc Harris <mharris@choicestream.com>
2020-11-01 19:16:48 -0500

    Write seven segment display information for unsigned decimal, signed decimal, unsigned hex, and unsigned octal

--- 8bit_cpu_output_eeprom.ino
+++ 8bit_cpu_output_eeprom.ino
@@ -18,6 +18,42 @@
 #define logWARNING "WARN"
 #define logERROR "EROR"
 
+#define ss_mid        (1 << 0)
+#define ss_top_left   (1 << 1)
+#define ss_low_left   (1 << 2)
+#define ss_low        (1 << 3)
+#define ss_low_right  (1 << 4)
+#define ss_top_right  (1 << 5)
+#define ss_top        (1 << 6)
+#define ss_dot        (1 << 7)
+
+#define ss_0  (   0    | ss_top_left | ss_top | ss_top_right | ss_low_left | ss_low | ss_low_right )
+#define ss_1  (   0    |      0      |   0    | ss_top_right |      0      |    0   | ss_low_right )
+#define ss_2  ( ss_mid |      0      | ss_top | ss_top_right | ss_low_left | ss_low |      0       )
+#define ss_3  ( ss_mid |      0      | ss_top | ss_top_right |      0      | ss_low | ss_low_right )
+#define ss_4  ( ss_mid | ss_top_left |   0    | ss_top_right |      0      |    0   | ss_low_right )
+#define ss_5  ( ss_mid | ss_top_left | ss_top |      0       |      0      | ss_low | ss_low_right )
+#define ss_6  ( ss_mid | ss_top_left | ss_top |      0       | ss_low_left | ss_low | ss_low_right )
+#define ss_7  (   0    |      0      | ss_top | ss_top_right |      0      |    0   | ss_low_right )
+#define ss_8  ( ss_mid | ss_top_left | ss_top | ss_top_right | ss_low_left | ss_low | ss_low_right )
+#define ss_9  ( ss_mid | ss_top_left | ss_top | ss_top_right |      0      | ss_low | ss_low_right )
+#define ss_A  ( ss_mid | ss_top_left | ss_top | ss_top_right | ss_low_left |    0   | ss_low_right )
+#define ss_B  ( ss_mid | ss_top_left |   0    |      0       | ss_low_left | ss_low | ss_low_right )
+#define ss_C  (   0    | ss_top_left | ss_top |      0       | ss_low_left | ss_low |      0       )
+#define ss_D  ( ss_mid |      0      |   0    | ss_top_right | ss_low_left | ss_low | ss_low_right )
+#define ss_E  ( ss_mid | ss_top_left | ss_top |      0       | ss_low_left | ss_low |      0       )
+#define ss_F  ( ss_mid | ss_top_left | ss_top |      0       | ss_low_left |    0   |      0       )
+#define ss_n  ( ss_mid |      0      |   0    |      0       |      0      |    0   |      0       )
+
+#define ss_units    (0)
+#define ss_tens     (1 << 8)
+#define ss_hundreds (1 << 9)
+#define ss_minus    ((1 << 8) | (1 << 9))
+#define ss_unsigned (0)
+#define ss_signed   (1 << 10)
+#define ss_hex      (0)
+#define ss_oct      (1 << 10)
+
 int logLevel = 0;
 
 void log(const char* level, const char *format, ...) {
@@ -122,19 +158,30 @@ void writeEEPROM(int address, byte data) {
 }
 
 void printContents() {
-  byte data[16];
-  for (int line = 0; line < 16; line +=1) {
-    int base = line * 16;
-    for (int offset = 0; offset < 16; offset += 1) {
+  int total = 2048;
+  int bytesPerBlock = 8;
+  int blocksPerRow = 4;
+  int bytesPerRow = bytesPerBlock * blocksPerRow;
+  int rows = total / bytesPerRow;
+  
+  byte data[bytesPerRow];
+  for (int line = 0; line < rows; line +=1) {
+    int base = line * bytesPerRow;
+    for (int offset = 0; offset < bytesPerRow; offset += 1) {
       data[offset] = readEEPROM(base + offset);
     }
 
     char buf[80];
-    sprintf(buf, "%03x:  %02x %02x %02x %02x %02x %02x %02x %02x    %02x %02x %02x %02x %02x %02x %02x %02x",
-            base,
-            data[0],  data[1],  data[2],  data[3],  data[4],  data[5],  data[6],  data[7],
-            data[8],  data[9],  data[10], data[11], data[12], data[13], data[14], data[15]);            
-    Serial.println(buf);
+    sprintf(buf, "%04x:", base);
+    Serial.print(buf);
+    for (int block = 0; block < blocksPerRow; ++block) {
+      Serial.print("   ");
+      for (int byt = 0; byt < bytesPerBlock; ++byt) {
+        sprintf(buf, "%02x ", data[(block * bytesPerBlock) + byt]);            
+        Serial.print(buf);
+      }
+    }
+    Serial.println("");
   }
 }
 
@@ -150,12 +197,46 @@ void setup() {
   Serial.println("");
 
   log(logINFO, "start");
-  
-  for (int i = 0; i < 256; i += 1) {
-    startLog(logDEBUG, "writing %2i to %03x", i, i);
-    writeEEPROM(i, i);
-    delay(10);
-    endLog(logDEBUG, "done");
+
+  byte digits[] = {ss_0, ss_1, ss_2, ss_3, ss_4, ss_5, ss_6, ss_7, ss_8, ss_9, ss_A, ss_B, ss_C, ss_D, ss_E, ss_F};
+
+  boolean decimal = false;
+
+  if (decimal) {
+    startLog(logINFO, "Writing unsigned decimal");
+    for (int value = 0; value < 256; ++value) {
+      writeEEPROM(value | ss_units    | ss_unsigned, digits[value % 10]);
+      writeEEPROM(value | ss_tens     | ss_unsigned, digits[(value / 10) % 10]);
+      writeEEPROM(value | ss_hundreds | ss_unsigned, digits[(value / 100) % 10]);
+      writeEEPROM(value | ss_minus    | ss_unsigned, 0);
+    }
+    endLog(logINFO, "done");
+    startLog(logINFO, "Writing signed decimal");
+    for (int value = -128; value < 128; ++value) {
+      writeEEPROM((value & 0xFF) | ss_units    | ss_signed, digits[abs(value) % 10]);
+      writeEEPROM((value & 0xFF) | ss_tens     | ss_signed, digits[(abs(value) / 10) % 10]);
+      writeEEPROM((value & 0xFF) | ss_hundreds | ss_signed, digits[(abs(value) / 100) % 10]);
+      writeEEPROM((value & 0xFF) | ss_minus    | ss_signed, (value < 0) ? ss_n : 0);
+    }
+    endLog(logINFO, "done");
+  }
+  else {
+    startLog(logINFO, "Writing unsigned hexadecimal");
+    for (int value = 0; value < 256; ++value) {
+      writeEEPROM(value | ss_units    | ss_hex, digits[abs(value) % 16]);
+      writeEEPROM(value | ss_tens     | ss_hex, digits[(abs(value) / 16) % 16]);
+      writeEEPROM(value | ss_hundreds | ss_hex, 0);
+      writeEEPROM(value | ss_minus    | ss_hex, 0);
+    }
+    endLog(logINFO, "done");
+    startLog(logINFO, "Writing unsigned octal");
+    for (int value = 0; value < 256; ++value) {
+      writeEEPROM(value | ss_units    | ss_oct, digits[abs(value) % 8]);
+      writeEEPROM(value | ss_tens     | ss_oct, digits[(abs(value) / 8) % 8]);
+      writeEEPROM(value | ss_hundreds | ss_oct, digits[(abs(value) / 64) % 8]);
+      writeEEPROM(value | ss_minus    | ss_oct, 0);
+    }
+    endLog(logINFO, "done");
   }
 
   printContents();
